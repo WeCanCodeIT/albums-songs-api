a9e6056c3e0dadbab2b36d5396ef824384b32838
Ben Williams <ben@wecancodeit.org>
2021-02-02 17:30:36 -0500

    Fixing code to pass testss

--- src/main/java/com/hipsterheaven/music/Album.java
+++ src/main/java/com/hipsterheaven/music/Album.java
@@ -1,14 +1,23 @@
 package com.hipsterheaven.music;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.Collection;
-
+@Entity
 public class Album {
     private String title;
     private String artistName;
     private String recordLabel;
     private String description;
+    @Id
+    @GeneratedValue
     private Long id;
+    @OneToMany
     private Collection<Song> songs;
+
+    protected Album(){}
 
     public Album(String title, String artistName, String recordLabel, String description) {
         this.title = title;
