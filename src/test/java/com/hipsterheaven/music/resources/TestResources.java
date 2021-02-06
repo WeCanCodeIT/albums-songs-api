2870efed93309a3e997c1fac073ed640880f2784
Ben Williams <ben@wecancodeit.org>
2021-02-06 03:24:10 -0500

    API finished

--- src/test/java/com/hipsterheaven/music/resources/TestResources.java
+++
@@ -1,7 +0,0 @@
-package com.hipsterheaven.music.resources;
-
-public class TestResources {
-    public static final Album TEST_ALBUM = new Album("Testing in Test Town", "Testy McTesterson", "Exam Records",
-            "Testing the night away.");
-    public static final Song TEST_SONG = new Song("Sample Song", "4:31", 5, TEST_ALBUM);
-}
